1071ad8e6fd78003f5a4bf1f199cf2794a89777c
Brian Temu <bryangasper2124@gmail.com>
2022-09-28 12:26:10 +0300

    desing changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     fontFamily: {
-      'sans': ['Poppins', 'sans-serif'],
+      sans: ["Poppins", "sans-serif"],
+      jet: ["JetBrains Mono", "sans-serif"],
     },
     extend: {},
   },
